9900ca33ec4736ad1fc7f9396a363d7afa8e0b0a
Nestor Larenas <sne.larenasortega@hotmail.com>
2023-10-30 16:18:22 -0300

    Selects añadidos a partials

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,10 +43,52 @@ public class HomeController : Controller
     [Route("products/showproduct/{ProductId}")]
     public IActionResult ShowProduct(int ProductId)
     {
+        List<Category> allCategories = _context.Categories.ToList();
+        ViewBag.AllCategories = allCategories;
+
+        Product? productWithCategory = _context.Products.Include(product => product.Categories)
+                                                        .ThenInclude(association => association.Category)
+                                                        .FirstOrDefault();
+
+
+        if (allCategories != null)
+        {
+            Console.WriteLine(allCategories);
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
         Product? OneProduct = _context.Products.FirstOrDefault(p => p.ProductId == ProductId);
-        ViewBag.AllCategories = _context.Categories.ToList();
         return View(OneProduct);
     }
+
+    [HttpPost]
+    [Route("products/addproducttocategory")]
+    public IActionResult AddProductToCategory(int productId, int categoryId)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == productId);
+        var category = _context.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
+
+        if (product != null && category != null)
+        {
+            var association = new Association
+            {
+                ProductId = productId,
+                CategoryId = categoryId
+            };
+
+            _context.Associations.Add(association);
+            _context.SaveChanges();
+        }
+        else
+        {
+            return View("ShowProduct");
+        }
+
+        return RedirectToAction("Index");
+    }
+
 
     [HttpGet]
     [Route("categories")]
@@ -76,6 +118,22 @@ public class HomeController : Controller
     [Route("categories/showcategory/{CategoryId}")]
     public IActionResult ShowCategory(int CategoryId)
     {
+        List<Product> allProducts = _context.Products.ToList();
+        ViewBag.AllProducts = allProducts;
+
+        Category? categoryWithProducts = _context.Categories.Include(category => category.Products)
+                                                            .ThenInclude(association => association.Product)
+                                                            .FirstOrDefault();
+
+
+        if (allProducts != null)
+        {
+            Console.WriteLine(allProducts);
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
         Category? OneCategory = _context.Categories.FirstOrDefault(c => c.CategoryId == CategoryId);
         return View(OneCategory);
     }
